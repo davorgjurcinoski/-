fc531b66e0f19c769f170f1ec3f452f2ecb5091c
saragligorova <saragligorova9@gmail.com>
2022-12-09 00:02:56 +0100

    adding a video

--- Domasna 2/tech prototype/dians/WP_Improved_Labs/WP_Improved_Labs/src/main/java/mk/finki/ukim/mk/lab/service/AtmService.java
+++
@@ -1,8 +0,0 @@
-package mk.finki.ukim.mk.lab.service;
-
-import mk.finki.ukim.mk.lab.model.Bank;
-
-public interface AtmService {
-
-    public Bank findClosestBank (Double lan, Double lon, String name);
-}
