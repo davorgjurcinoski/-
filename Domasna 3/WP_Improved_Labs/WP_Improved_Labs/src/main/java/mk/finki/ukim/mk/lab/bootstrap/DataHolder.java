58a2d944447bc535b28694897440fd20a1e59712
Davor <84294947+davorgjurcinoski@users.noreply.github.com>
2022-12-16 18:48:56 +0100

    added changes

---
+++ Domasna 3/WP_Improved_Labs/WP_Improved_Labs/src/main/java/mk/finki/ukim/mk/lab/bootstrap/DataHolder.java
@@ -0,0 +1,29 @@
+package mk.finki.ukim.mk.lab.bootstrap;
+
+import mk.finki.ukim.mk.lab.model.Bank;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class DataHolder {
+    public static List<Bank> banks = new ArrayList<>();
+
+
+
+    @PostConstruct
+    public void init() {
+
+    Bank bank1 = new Bank((long) 1,41.9936657,21.4428736,"НБРМ");
+    Bank bank2 = new Bank((long) 2,42.0073792,21.3682515,"Stopanska banka");
+    Bank bank3 = new Bank((long) 3,41.9876902,21.4327574,"Стопанска Банка");
+
+    banks.add(bank1);
+    banks.add(bank2);
+    banks.add(bank3);
+
+
+   }
+}
