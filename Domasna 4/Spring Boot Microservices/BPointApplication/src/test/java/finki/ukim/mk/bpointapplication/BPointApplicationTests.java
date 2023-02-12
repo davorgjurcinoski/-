d02bc29f8c014e129355e98ff30384c59548fe54
saragligorova <saragligorova9@gmail.com>
2023-02-12 16:25:16 +0100

    new microservice

---
+++ Domasna 4/Spring Boot Microservices/BPointApplication/src/test/java/finki/ukim/mk/bpointapplication/BPointApplicationTests.java
@@ -0,0 +1,13 @@
+package finki.ukim.mk.bpointapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BPointApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
